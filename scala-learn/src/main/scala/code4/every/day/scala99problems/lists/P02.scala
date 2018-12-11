930d81fb046f278ec2e5f9cfdbe3b1088e3b49cf
Almaz Murzabekov <muktaalmaz@yandex.ru>
2018-12-11 22:27:07 +0300

    P02 logic implemented

--- scala-learn/src/main/scala/code4/every/day/scala99problems/lists/P02.scala
+++ scala-learn/src/main/scala/code4/every/day/scala99problems/lists/P02.scala
@@ -2,6 +2,11 @@ package code4.every.day.scala99problems.lists
 
 trait P02 {
 
-  def penultimate[A](list: List[A]): Option[A] = ???
+  def penultimate[A](list: List[A]): Option[A] = {
+    if (list == null || list.isEmpty)
+      None
+    else
+      Some(list.takeRight(2).head)
+  }
 
 }
