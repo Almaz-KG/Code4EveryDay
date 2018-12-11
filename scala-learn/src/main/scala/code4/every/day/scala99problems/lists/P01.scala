4c8822e59ec088e2ceb67fa144e5254cd7607d24
Almaz Murzabekov <muktaalmaz@yandex.ru>
2018-12-11 21:48:13 +0300

    P01 logic implemented

--- scala-learn/src/main/scala/code4/every/day/scala99problems/lists/P01.scala
+++ scala-learn/src/main/scala/code4/every/day/scala99problems/lists/P01.scala
@@ -2,5 +2,9 @@ package code4.every.day.scala99problems.lists
 
 trait P01 {
 
-  def last[A](list: List[A]): Option[A] = ???
+  def last[A](list: List[A]): Option[A] = {
+    if (list != null && list.nonEmpty)
+      Some(list.last)
+    else None
+  }
 }
